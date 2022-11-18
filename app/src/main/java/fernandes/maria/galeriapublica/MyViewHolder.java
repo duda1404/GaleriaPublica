e49f116c7537e4bcd3a1949170188ce5b06994a8
Maria_Eduarda_Silva <87455625+duda1404@users.noreply.github.com>
2022-11-18 10:04:34 -0300

    Ta com problema no GridViewFragment

---
+++ app/src/main/java/fernandes/maria/galeriapublica/MyViewHolder.java
@@ -0,0 +1,11 @@
+package fernandes.maria.galeriapublica;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(View v) {
+        super(v);
+    }
+}
